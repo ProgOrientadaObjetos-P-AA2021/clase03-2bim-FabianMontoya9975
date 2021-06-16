fa01a69a0b09422a3eebaf0efbb501b8aae3a52b
Fabián Montoya <jfmontoya1@utpl.edu.ec>
2021-06-16 16:15:47 -0500

    Resolución problematica 1

--- EjemploHerencia/Herencias/src/herencias2/Principal.java
+++ EjemploHerencia/Herencias/src/herencias2/Principal.java
@@ -5,6 +5,8 @@
  */
 package herencias2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -16,18 +18,70 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Estudiante e = new Estudiante("René", "Elizalde", 33, 100.2);
-        System.out.println(e);
+        Scanner entrada = new Scanner(System.in);
+        int op;
+        String nombre;
+        String apellido;
+        int edad;
+        // menú
+        System.out.println("Opción 1: Ingresa estudiante.\n"
+                + "Opción 2: Ingresa docente.\n"
+                + "opción 3: Ingresa policia.");
+        op = entrada.nextInt();
+        entrada.nextLine();
+        if ((op >= 1) && (op <= 3)) {
+            System.out.println("Ingrese el nombre: ");
+            nombre = entrada.nextLine();
+            System.out.println("Ingrese el apellido: ");
+            apellido = entrada.nextLine();
+            System.out.println("Ingrese la edad: ");
+            edad = entrada.nextInt();
+            switch (op) {
+                case 1:
+                    ingresarEstudiante(nombre, apellido, edad);
+                    break;
+                case 2:
+                    ingresarDocente(nombre, apellido, edad);
+                    break;
+                case 3:
+                    ingresarPolicia(nombre, apellido, edad);
+                    break;
+            }
+        } else {
+            System.out.println("Opción incorrecta");
+        }
+
+    }
+
+    public static void ingresarEstudiante(String a, String b, int c) {
+        Scanner entrada = new Scanner(System.in);
+        double matricula;
+        System.out.println("Ingrese el valor de la matícula");
+        matricula = entrada.nextDouble();
         // Un docente hereda de una Persona y adicionalmente tiene 
         // la característia sueldo
-        Docente d = new Docente("Luis", "Alvarez", 40, 1000); // falta implementar
+        Estudiante e = new Estudiante(a, b, c, matricula);
+        System.out.println(e);
+    }
+
+    public static void ingresarDocente(String a, String b, int c) {
+        Scanner entrada = new Scanner(System.in);
+        double sueldo;
+        System.out.println("Ingrese el sueldo: ");
+        sueldo = entrada.nextDouble();
+        Docente d = new Docente(a, b, c, sueldo); // falta implementar
         System.out.println(d);
-        
+    }
+
+    public static void ingresarPolicia(String a, String b, int c) {
+        Scanner entrada = new Scanner(System.in);
+        String rango;
+        System.out.println("Ingrese el rango: ");
+        rango = entrada.nextLine();
         // Un policia hereda de una Persona y adicionalmente tiene 
         // la característia rango
-        Policia p = new Policia("ALEX", "MENDOZA", 35, "Cabo"); // falta implementar
+        Policia p = new Policia(a, b, c, rango); // falta implementar
         System.out.println(p);
-        
     }
-    
+
 }
